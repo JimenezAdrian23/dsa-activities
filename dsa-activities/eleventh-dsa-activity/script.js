36adf17c6d7006864fc05797385b1929e8eaccd7
JimenezAdrian23 <147502990+JimenezAdrian23@users.noreply.github.com>
2023-11-08 21:04:04 +0800

    Add files via upload

---
+++ dsa-activities/eleventh-dsa-activity/script.js
@@ -0,0 +1,10 @@
+//Create the function named myFunction
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+//Create a variable and pass your function
+var result = myFunction(5, 7);
+
+//Log the value (call the variable name)
+console.log(result);
